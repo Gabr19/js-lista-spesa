4ed7e8a333bff8d5a7f2bbd442236f0214f443d2
Gabr19 <sannagabriele19@gmail.com>
2023-01-10 16:19:46 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,13 @@ const items = ['latte', 'uova', 'pane']
 const itemsList = document.getElementById('items')
 i = 0
 while (i < items.length){
+    console.log(i, items[i]);
     itemsList.innerHTML += `<li>${items[i]}</li>` ;
+    
+    
+    const newLi = document.createElement('li');
+    newLi.innerHTML = items[i]
+    itemsList.append(newLi);
 
+    i++
     }
